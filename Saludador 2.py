fae6028f6bb5b3202e0e377faf4de6b7636f4571
p-sanzbeltran <55576859+p-sanzbeltran@users.noreply.github.com>
2019-10-15 12:04:17 +0200

    Add files via upload

---
+++ Saludador 2.py
@@ -0,0 +1,6 @@
+ def saludador_():
+	print "Hola amigo" #Instrucciones de salida
+	Nombre= raw_input("Como te llamas?: ") #Instrucción de entrada
+	print "Bienvenido a Python"+ nombre
+
+saludador_2()
