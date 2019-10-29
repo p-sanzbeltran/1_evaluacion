312865f8f3e63991225b808d46fc13925c0d4951
p-sanzbeltran <55576859+p-sanzbeltran@users.noreply.github.com>
2019-10-29 11:25:02 +0100

    Delete Lista.py

--- Lista.py
+++
@@ -1,7 +0,0 @@
-def ejemplo_for():
-    #i es una variable que toma los valores comprendidos en el rango
-    final=input("Hasta que numero: ")
-    for i in range (1,100):
-    if a/i
-    
-ejemplo_for()
