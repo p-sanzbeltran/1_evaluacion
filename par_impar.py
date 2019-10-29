233fe55aa9900efaa294830db6f9872c39d66051
p-sanzbeltran <55576859+p-sanzbeltran@users.noreply.github.com>
2019-10-29 11:27:21 +0100

    Add files via upload

---
+++ par_impar.py
@@ -0,0 +1,8 @@
+#queremos que el programa sepa si un numero es par o impar
+def par_impar():
+    a=input("añade un numero: ")
+    if a%2==0:
+        print "El numero es par"
+    else:
+        print "El numero es impar"
+par_impar()
