f0dac3e8936d203fcfd05dfdc7cab5673acab244
p-sanzbeltran <55576859+p-sanzbeltran@users.noreply.github.com>
2019-10-29 11:25:15 +0100

    Delete Saludador 2.py

--- Saludador 2.py
+++
@@ -1,6 +0,0 @@
- def saludador_():
-	print "Hola amigo" #Instrucciones de salida
-	Nombre= raw_input("Como te llamas?: ") #Instrucción de entrada
-	print "Bienvenido a Python"+ nombre
-
-saludador_2()
